9d65581b2f16a2546a80e7ff8f28309d8f9881aa
Antonio Pagano <apagano@wawand.co>
2016-02-11 11:48:23 -0500

    [feature] init command

--- commands/init.go
+++ commands/init.go
@@ -1,9 +1,12 @@
 package commands
 
 import (
+	"bytes"
+	"errors"
 	"log"
 	"os"
 	"path"
+	"text/template"
 
 	"github.com/codegangsta/cli"
 )
@@ -31,30 +34,73 @@ var InitAction = func(c *cli.Context) {
 	}
 
 	initParams.Name = c.Args()[0]
+	err := createFolder()
+	if err != nil {
+		return
+	}
+
+	createSubfolders()
+	createFiles()
+}
+
+func createFolder() error {
 	projectPath := path.Join(initParams.DestionationPath, initParams.Name)
 
 	if exists, _ := pathExists(projectPath); exists == true {
 		log.Println("| ERROR: Folder already exist.")
-		return
-	}
-
-	err := os.Mkdir(projectPath, 0777)
-	if err != nil {
-		log.Println("| ERROR: Could not create directory: ", err.Error())
-		return
+		return errors.New("Folder already exist.")
 	}
 
 	//Create {Path}/{AppName} folder
-	//Create folders inside {Path}/{AppName}
-	//Create Files based on templates
-}
+	err := os.Mkdir(projectPath, 0777)
+	if err != nil {
+		log.Println("| ERROR: Could not create directory: ", err.Error())
+		return err
+	}
 
-func InitProject(c *cli.Context) error {
 	return nil
 }
 
+func createSubfolders() {
+	//Create folders inside {Path}/{AppName}
+	projectPath := path.Join(initParams.DestionationPath, initParams.Name)
+	folders := []string{
+		path.Join(projectPath, "handlers"),
+		path.Join(projectPath, "middlewares"),
+	}
+
+	for _, folder := range folders {
+		err := os.Mkdir(folder, 0777)
+		if err != nil {
+			log.Println("| ERROR: Could not create directory: ", err.Error())
+		}
+	}
+}
+
+func createFiles() {
+	projectPath := path.Join(initParams.DestionationPath, initParams.Name)
+	//Create Files based on templates
+	files := map[string]string{
+		path.Join(projectPath, "handlers", "sample.go"):   handlersTemplate,
+		path.Join(projectPath, "middlewares", ".gitkeep"): ".gitkeep",
+		path.Join(projectPath, "routes.go"):               routesTemplate,
+	}
+
+	for path, content := range files {
+		file, err := os.Create(path)
+		if err != nil {
+			log.Println("| ERROR: Could not create file: ", err.Error())
+			continue
+		}
+
+		buf := new(bytes.Buffer)
+		template.Must(template.New("sample").Parse(content)).Execute(buf, initParams)
+		file.WriteString(buf.String())
+	}
+}
+
 var routesTemplate = `package main
-import "{.Name}/handlers"
+import "{{.Name}}/handlers"
 
 //defineRoutes gets called by the generated main.go, please define routes inside it.
 func defineRoutes(r *Router){
